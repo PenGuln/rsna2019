9b5d3f9dfdc03b32a765213461aa78ad6c31acaa
Siqi Guo <siqi@tamu.edu>
2024-02-24 22:49:33 -0600

    first commit

---
+++ 2DNet/src/settings.py
@@ -0,0 +1,2 @@
+train_png_dir = r'/scratch/group/optmai/datasets/rsna19/train_png/'
+test_png_dir = r'/scratch/group/optmai/datasets/rsna19/test_png/'
---
+++ prepare.sh
@@ -0,0 +1,1 @@
+python ./2DNet/src/prepare_data.py -dcm_path /scratch/group/optmai/datasets/rsna19/input/stage_2_train_images -png_path /scratch/group/optmai/datasets/rsna19/train_png
